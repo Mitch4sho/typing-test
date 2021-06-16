9d5c3aa29b5c0c31803176de009abe739b58e642
Mitchell Oliver <mitchelloliver@Mitchells-Mac-mini.local>
2021-06-16 09:44:45 -0700

    Added the featur to start and stop the timer this will be on a onchange event, added in a reset button to reset state

--- src/TypingTest.js
+++ src/TypingTest.js
@@ -1,32 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect}from 'react';
 
 
-//  A reset button
-
+// how to build a box for you to type in with words in it?
+// how to put a underline for each charcter
+// how to check if the character is clicked
+// check if the sentence length is reached
 //check if the key is being macthed with the charcter
 
-// start timer
-// check if the sentence length is reached
-// end timer
 
-// logic 
-// get the character in numbers and divided by the difference
-// between start and end timer
 
 function TypingTest () {
-    // state 
-    // start and end timestamp
-    // current sentence which will be a array
-    // current postions in the sentence index
+    // we separte the states to make it easier to work with
+    const [time, setTime] = useState(0)
+    const [timerOn, setTimeOn] = useState(false)
+    const [sentence, setSentence] = useState([]); // maybe have array of sentence
+    
+    useEffect(() => {
+        console.log(timerOn)
+        let interval = null;
+        if (timerOn) {
+            interval = setInterval(() => {
+                setTime(prevTime => prevTime + 1) 
+            }, 1000)
+        } else {
+            clearInterval(interval)
+        }
+        
+        return () => clearInterval(interval)
+    }, [timerOn])  
+    // set new state for end time
+    // logic 
+    // get the character in numbers and divided by the difference
+    
+    const reset = () => {
+        setTime(0);
+        setTimeOn(false)
+        // setSentence() /// set to a random number for the index
+    }
+    //  A reset button
     return (
-        <div>
-            Hello
+        <div> 
             {/* Display Timer  */}
+            <h2>{time}</h2>
 
-            {/* Have a display of current / 
+            {/* Have a display of current cahrarchter
             get a underline under current  character*/}
 
             {/* display character per second */}
+
+            {/* need to add a start and stop timer to onChange */}
+            <button onClick={() => setTimeOn(!timerOn)}>Press Me</button> 
+            <button onClick={() => reset()}>Reset Typing test</button>
         </div>
     )
 }
