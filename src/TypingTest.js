8e155377dfe4ce920b6b675ce67bc7811f0e2fa0
Mitchell Oliver <mitchelloliver@Mitchells-Mac-mini.local>
2021-06-15 17:58:09 -0700

    added the plan for the project

---
+++ src/TypingTest.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+
+//  A reset button
+
+//check if the key is being macthed with the charcter
+
+// start timer
+// check if the sentence length is reached
+// end timer
+
+// logic 
+// get the character in numbers and divided by the difference
+// between start and end timer
+
+function TypingTest () {
+    // state 
+    // start and end timestamp
+    // current sentence which will be a array
+    // current postions in the sentence index
+    return (
+        <div>
+            Hello
+            {/* Display Timer  */}
+
+            {/* Have a display of current / 
+            get a underline under current  character*/}
+
+            {/* display character per second */}
+        </div>
+    )
+}
+
+export default TypingTest;
